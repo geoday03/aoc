a7b4b525758afa915a22b6902316b27752945610
geoday03 <ge0day@protonmail.com>
2021-12-01 03:26:56 -0800

    renamed variable

--- 2021/days/1/increasedDepthCount.js
+++ 2021/days/1/increasedDepthCount.js
@@ -4,10 +4,10 @@ const rl = require("readline/promises").createInterface({
 
 var lastDepth
 var greaterDepthCount = 0
-rl.on("line", line => {
-    if (Number(line) > lastDepth) {
+rl.on("line", depth => {
+    if (Number(depth) > lastDepth) {
         greaterDepthCount++
     }
 
-    lastDepth = Number(line)
+    lastDepth = Number(depth)
 }).on("close", () => { console.log(greaterDepthCount) })
