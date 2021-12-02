b208db01466ea2e18071899029aeb878a2f56a97
geoday03 <71962923+geoday03@users.noreply.github.com>
2021-12-01 22:44:00 -0800

    Update part1.py

--- 2021/days/2/part1.py
+++ 2021/days/2/part1.py
@@ -1,13 +1,13 @@
 lines = open("./input.txt", "r").read().split("\n")
 
-forward = 0
+horizontal = 0
 depth = 0
 for line in lines:
     if line.startswith("forward"):
-        forward += int(line[-1])
+        horizontal += int(line[-1])
     elif line.startswith("up"):
         depth -= int(line[-1])
     elif line.startswith("down"):
         depth += int(line[-1])
 
-print(forward * depth)
+print(horizontal * depth)
